eeb86f50c2848c6449e6ca147c133b4bfa0b8b58
Austin <adjansky@charter.net>
2023-11-19 22:23:52 -0600

    FInishing the admin pages

--- Administrator/EditInstructor.aspx.cs
+++ Administrator/EditInstructor.aspx.cs
@@ -12,45 +12,69 @@ namespace Assignment4Test.Administrator
     public partial class EditInstructor : System.Web.UI.Page
     {
 
+        //Creating data contexts for the database
         KarateDataContext dbcon;
         KarateDataContext dbcon2;
+
+        //Creating the connection string
         string conn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Austin\\Desktop\\AAATEST_Nov17\\AAATEST_Nov17\\App_Data\\KarateSchool.mdf;Integrated Security=True;Connect Timeout=30";
 
+        //Creating the page load event
         protected void Page_Load(object sender, EventArgs e)
         {
             ShowAll();
         }
 
+        //Creating a helper method for refreshing the grid view
         public void ShowAll()
         {
+            //Assigning the context a value
             dbcon = new KarateDataContext(conn);
+
+            //Creating a variable for bringing data to the gridview
             var result = from x in dbcon.Members
                          select x;
+
+            //Assigning a value and binding the grid view 
             GridView1.DataSource = result;
             GridView1.DataBind();
         }
 
+        //Creating the button click event
         protected void Button1_Click(object sender, EventArgs e)
         {
 
+            //Creating a connection with the connection string
             using (SqlConnection connection = new SqlConnection(conn)) 
             {
+                //Creating a variable to get the first name
                 string fName = TextBox1.Text;
+
+                //Creating a query for deleting a member from the table
                 string deleteQuery = $"DELETE FROM Member WHERE MemberFirstName = @fName";
 
+                //Creating a sql command 
                 using (SqlCommand command = new SqlCommand(deleteQuery, connection))
                 {
+
+                    //Opening the connection
                     connection.Open();
+
+                    //Adding the fName to the parameter
                     command.Parameters.AddWithValue("@fName", fName);
 
+                    //Using int to retrieve the rows affected
                     int rowsAffected = command.ExecuteNonQuery();
 
+                    //Using if/else to detect if a change was made 
                     if (rowsAffected > 0)
                     {
+                        //Showing a message if a row is deleted
                         Console.WriteLine("Done.");
                     }
                     else
                     {
+                        //Showing a message if no row is deleted
                         Console.WriteLine("No rows deleted.");
                     }
 
--- Administrator/SectionPage.aspx.cs
+++ Administrator/SectionPage.aspx.cs
@@ -12,39 +12,67 @@ namespace Assignment4Test.Administrator
     public partial class SectionPage : System.Web.UI.Page
     {
 
+        //Creating the data contexts for the database
         KarateDataContext dbcon;
         KarateDataContext dbcon2;
         
+        //Creating the connection string
         string conn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Austin\\Desktop\\AAATEST_Nov17\\AAATEST_Nov17\\App_Data\\KarateSchool.mdf;Integrated Security=True;Connect Timeout=30";
         
+        //Creating the page load event
         protected void Page_Load(object sender, EventArgs e)
         {
             ShowAll();
         }
 
-
+        //Creating a helper method for refreshing the database
         public void ShowAll()
         {
+            //Assigning a value to the database context
             dbcon = new KarateDataContext(conn);
+            
+            //Creating a result to show in the grid view
             var result = from x in dbcon.Members
                          select x;
+            
+            //Assigning the result and binding it to the grid view
             GridView1.DataSource = result;
             GridView1.DataBind();
         }
 
+
+        //Creating the button click event
         protected void Button1_Click(object sender, EventArgs e)
         {
 
+            //Creating a connection with the connection string
             using (SqlConnection connection = new SqlConnection(conn))
             {
+                //Creating variables for the memberID and the sectionID
                 int memberIDToAdd = Convert.ToInt32(TextBox1.Text);
                 int chosenSectionID = Convert.ToInt32(TextBox2.Text);
-                string addQuery = $"INSERT INTO Section (Member_ID) VALUES (" 
-                    
 
-                //using (SqlCommand)
+                //Creating a query for adding a member to a section
+                string addQuery = $"INSERT INTO Section (Member_ID) VALUES (@memberIDToAdd) WHERE SectionID = @chosenSectionID";
+                
+                //Creating a command with the query
+                using (SqlCommand command = new SqlCommand(addQuery, connection)) 
+                {
+                    //Opening the connection
+                    connection.Open();
+
+                    //Filling in the parameters for the add query
+                    command.Parameters.AddWithValue("@memberIDToAdd", memberIDToAdd);
+                    command.Parameters.AddWithValue("chosenSectionID", chosenSectionID);
+
+                    //Executing the query
+                    command.ExecuteNonQuery();
+                }
+
             }
 
+            //Refreshing the grid view 
+            ShowAll();
         }
     }
 }
